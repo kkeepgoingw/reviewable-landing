47bb3c5802b50912d7758a9e789a7e8d5dc90776
sy.kim <sy.kim@reviewable.kr>
2025-05-19 13:35:00 +0900

    chore: GitHub Pages 배포를 위한 빌드 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   output: "export", // next export 사용 가능하게 설정
   trailingSlash: true, // GitHub Pages에서 경로 문제 방지
+  images: {
+    unoptimized: true
+  },
   assetPrefix: isProd ? "./" : undefined, // 선택 사항: 상대 경로 처리
 };
 
