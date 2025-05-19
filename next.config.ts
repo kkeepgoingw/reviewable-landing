3d038a77c895bd9bc0baa8e8712e9710bfc766b6
sy.kim <sy.kim@reviewable.kr>
2025-05-19 13:12:38 +0900

    chore: gh-pages, deploy scripts 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export", // next export 사용 가능하게 설정
+  trailingSlash: true, // GitHub Pages에서 경로 문제 방지
+  assetPrefix: isProd ? "./" : undefined, // 선택 사항: 상대 경로 처리
 };
 
 export default nextConfig;
